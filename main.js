292a6ff79f17191ce8c941b9d1c98bfbc9f2d7a8
elianderson-dev <nin2333124568@gmail.com>
2023-01-01 19:23:22 +0000

    feat: adds the style and modifier add function

--- main.js
+++ main.js
@@ -2,5 +2,12 @@ function rollDice(a, b){
     return Math.floor(Math.random() * (b - a + 1)) + a
 }
 function showNumber(){
-    document.getElementById("diceNumber").innerHTML=rollDice(1, 20)
+    var diceNumber = document.getElementById("diceNumber").innerHTML=rollDice(1, 20)
+    var takeModifier = document.getElementById("mod").value
+    var sumMod = parseInt(takeModifier)+parseInt(diceNumber)
+    document.getElementById("diceTotal").innerHTML="Total:"+sumMod
 }
+function dark(){
+    var dark = document.body;
+    dark.classList.toggle("dark")
+    }
