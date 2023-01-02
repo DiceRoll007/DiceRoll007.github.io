e98979d21adaebe86a412fc2e0f93d82ba0318ae
elianderson-dev <nin2333124568@gmail.com>
2023-01-02 10:42:16 +0000

    feat: adds other dice values

--- main.js
+++ main.js
@@ -1,33 +1,52 @@
 function rollDice(a, b){
     return Math.floor(Math.random() * (b - a + 1)) + a
 }
+
 function showNumber(){
     var diceNumber = document.getElementById("diceNumber").innerHTML=rollDice(1, 20)
     var takeModifier = document.getElementById("mod").value
     var sumMod = parseInt(takeModifier)+parseInt(diceNumber)
     document.getElementById("diceTotal").innerHTML="Total:"+sumMod
-    var img = document.getElementById("diceImg");
-    img.setAttribute("src", "images/d20.png")
 }
 
 function changeDice(){
     var takeDice = document.getElementById("changeDice").value
     if (takeDice == "4"){
         document.getElementById("diceImg").src="images/d4.png"
+        var diceNumber = document.getElementById("diceNumber").innerHTML=rollDice(1, 4)
+        var takeModifier = document.getElementById("mod").value
+        var sumMod = parseInt(takeModifier)+parseInt(diceNumber)
+        document.getElementById("diceTotal").innerHTML="Total:"+sumMod
     }
     if (takeDice == "6"){
         document.getElementById("diceImg").src="images/d6.png"
+        var diceNumber = document.getElementById("diceNumber").innerHTML=rollDice(1, 6)
+        var takeModifier = document.getElementById("mod").value
+        var sumMod = parseInt(takeModifier)+diceNumber
+        document.getElementById("diceTotal").innerHTML="Total:"+sumMod
     }
     if (takeDice == "8"){
         document.getElementById("diceImg").src="images/d8.png"
+        var diceNumber = document.getElementById("diceNumber").innerHTML=rollDice(1, 8)
+        var takeModifier = document.getElementById("mod").value
+        var sumMod = parseInt(takeModifier)+diceNumber
+        document.getElementById("diceTotal").innerHTML="Total:"+sumMod
     }
     if (takeDice == "10"){
         document.getElementById("diceImg").src="images/d10.png"
+        var diceNumber = document.getElementById("diceNumber").innerHTML=rollDice(1, 10)
+        var takeModifier = document.getElementById("mod").value
+        var sumMod = parseInt(takeModifier)+diceNumber
+        document.getElementById("diceTotal").innerHTML="Total:"+sumMod
     }
     if (takeDice == "12"){
         document.getElementById("diceImg").src="images/d12.png"
+        var diceNumber = document.getElementById("diceNumber").innerHTML=rollDice(1, 12)
+        var takeModifier = document.getElementById("mod").value
+        var sumMod = parseInt(takeModifier)+diceNumber
+        document.getElementById("diceTotal").innerHTML="Total:"+sumMod
     }
-    else {
+    if (takeDice == "20"){
         document.getElementById("diceImg").src="images/d20.png"
     }
 }
